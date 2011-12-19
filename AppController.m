1654f21c723dadf0d88b0e92f882a6c9a69bf08e
Charles McGarvey <chazmcgarvey@brokenzipper.com>
2011-12-19 15:17:41 -0700

    update contact information and project URL

--- AppController.m
+++ AppController.m
@@ -1,9 +1,9 @@
 
 /*
  * The Cheat - The legendary universal game trainer for Mac OS X.
- * http://www.dogcows.com/chaz/wiki/TheCheat
+ * http://www.brokenzipper.com/trac/wiki/TheCheat
  *
- * Copyright (c) 2003-2010, Charles McGarvey et al.
+ * Copyright (c) 2003-2011, Charles McGarvey et al.
  *
  * Distributable under the terms and conditions of the 2-clause BSD
  * license; see the file COPYING for the legal text of the license.
